52986cd20d3e6dd30b5d81ea6109d5f3de49bb58
Albinosius <122452352+Albinosius@users.noreply.github.com>
2023-03-19 12:52:50 +0500

    w/h 4 week another problem

--- script/form.js
+++ script/form.js
@@ -1,31 +1,106 @@
-// попытка использования fancybox
+const validateFields = (form, fieldsArray) => {
 
-// $('.form').submit(e => {
-//   e.preventDefault();
+  fieldsArray.forEach((filed) => {
+    filed.removeClass("input-error");
+    if (filed.val().trim() == "") {
+      filed.addClass("input-error");
+    }
+  });
 
-//   $.fancybox.open({
-//     src: "#modal",
-//     type: "inline"
-//   });
-// })
+  const errorFields = form.find(".input-error");
 
-// $(".app-submit-btn").click(e => {
-//   e.preventDefault();
+  return errorFields.length == 0;
+}
 
-//   $fancybox.close();
-// })
-
-const modal = $('.modal');
-
-$('[type="submit"]').click(e => {
+$('.form').submit(e => {
   e.preventDefault();
-  modal.addClass("modal--active");
+
+  const form = $(e.currentTarget);
+  const name = form.find("[name='name']");
+  const phone = form.find("[name='phone']");
+  const comment = form.find("[name='comment']");
+  const to = form.find("[name='to']");
+
+  const modal = $('.modal');
+  const content = modal.find(".modal__content");
+  modal.removeClass("modal__content--error");
+
+
+  const isValid = validateFields(form, [name, phone, comment]);
+
+  const submit = form.find('[type="submit"]');
+  const submitClose = form.find('.app-submit-btn');
+
+  if (isValid) {
+    $.ajax({
+      url: "https://webdev-api.loftschool.com/sendmail",
+      method: "post",
+      data: {
+        name: name.val(),
+        phone: phone.val(),
+        comment: comment.val(),
+        to: to.val()
+      } 
+      // success: (data) => {
+      //   content.text(data.message);
+
+      //   console.log(data);
+
+      //   submit.click(e => {
+      //     e.preventDefault();
+      //     modal.addClass("modal--active");
+      //   });
+
+      //   submitClose.click(e => {
+      //     e.preventDefault();
+      //     modal.removeClass("modal--active");
+      //   })
+      // },
+      // error: (data) => {
+      //   const message = data.responseJSON.message;
+      //   console.log(message); //не работает
+      //   content.text(message);
+      // modal.addClass("modal__content--error");
+
+      // submit.click(e => {
+      //   e.preventDefault();
+      //   modal.addClass("modal--active");
+      // });
+
+      // submitClose.click(e => {
+      //   e.preventDefault();
+      //   modal.removeClass("modal--active");
+      // })
+      // }
+    });
+
+    
+    submit.addEventListener('click', () => {
+      const xhr = new XMLHttpRequest();
+      console.log(xhr);
+      if (xhr.status >= 400) {
+        content.innertext('Заказ принят на обработку');
+      } else {
+        content.innertext('Что-то пошло не так, попробуйте снова');
+        content.addClass("modal__content--error");
+      }
+    });
+  }
+
+  
+
+  submit.click(e => {
+    e.preventDefault();
+    modal.addClass("modal--active");
+  });
+
+  submitClose.click(e => {
+    e.preventDefault();
+    modal.removeClass("modal--active");
+  });
 });
 
-$('.app-submit-btn').click(e => {
-  e.preventDefault();
-  modal.removeClass("modal--active");
-})
+
 
 
 
